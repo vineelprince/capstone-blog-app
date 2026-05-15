8fae48cd54f4c0be12c6a726997db7d21b8a44e8
vineel <vineel183sainik@gmail.com>
2026-05-15 09:35:50 +0530

    Update CORS configuration to allow multiple origins

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,11 @@ config(); //process.env
 const app = exp();
 //use cors middleware
 app.use(cors({
-  origin: 
-    "http://localhost:5173",
+ origin: [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://full-stack-blog-app-vz3g.vercel.app"
+],
   credentials: true
 }));
 
